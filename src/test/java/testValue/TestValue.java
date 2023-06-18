acce5e0dd32636af49e08edc4f0798f0313f2834
Andrey Vorobev <mamin.xaker@yandex.ru>
2023-06-18 12:37:48 +0300

    Add test value and OrderStellar

---
+++ src/test/java/testValue/TestValue.java
@@ -0,0 +1,18 @@
+package testValue;
+
+/**
+ * Содержит значения для тестов, такие как: login, password и т.д
+ */
+public class TestValue {
+    public static final String
+            TEST_LOGIN_ONE = "james-bond@yandex.ru",
+            TEST_LOGIN_TWO = "scroogeMcDuck@yandex.ru",
+            TEST_PASSWORD_ONE = "123",
+            TEST_PASSWORD_TWO = "123qwe",
+            TEST_NAME_ONE = "james-bond",
+            TEST_NAME_TWO = "scroogeMcDuck",
+            TEST_BUN = "61c0c5a71d1f82001bdaaa6d",
+            TEST_BAD_BUN = "123",
+            TEST_FILLING_ONE = "61c0c5a71d1f82001bdaaa7a",
+            TEST_FILLING_TWO = "61c0c5a71d1f82001bdaaa76";
+}
