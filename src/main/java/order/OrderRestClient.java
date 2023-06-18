9929083bef2e057b335a046e300f65bdcc742ef0
Andrey Vorobev <mamin.xaker@yandex.ru>
2023-06-18 13:09:08 +0300

    Add OrderClient, OrderRestClient

--- src/main/java/order/OrderRestClient.java
+++ src/main/java/order/OrderRestClient.java
@@ -1,12 +1,12 @@
-package user;
+package order;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 /**
- * Содержит основную ссылку на Stellar Burgers для пользователя и настройку через getBaseReqSpec
+ * Содержит основную ссылку на Stellar Burgers для заказа и настройку через getBaseReqSpec
  */
-public class UserRestClient {
+public class OrderRestClient {
     protected static final String BASE_URI = "https://stellarburgers.nomoreparties.site";
 
     protected RequestSpecification getBaseReqSpec() {
